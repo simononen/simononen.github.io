7a76dc41a7af7442dc631523ce0a809f93b436b5
Simon Onen <sonen8524@gmail.com>
2020-05-03 07:11:10 +0300

    Update index.html file

--- content/post001.md
+++ content/post001.md
@@ -1,7 +1,7 @@
 ---
 noteId: "4c5e4ab08c7d11ea80de21e029e0d05d"
 tags: []
-title: First Post on My Sweet New Blog
+title: First Sample Post
 date: 02-05-2020
 author: Onen Simon
 
--- pelicanconf.py
+++ pelicanconf.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'Onen Simon (@sonensimon)'
+AUTHOR = 'Simon Onen'
 SITENAME = 'Data Cosines'
 SITEURL = ''
 
