eca07ca3c4e43b8bbf7bef2f3458a2fec6583085
Simon Onen <sonen8524@gmail.com>
2021-02-13 21:33:29 +0300

    Chore: Add new layout

--- simonblog/src/app/app.component.ts
+++ simonblog/src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { concatMap, delay, repeat } from 'rxjs/operators';
+​
+import TypeIt from 'typeit';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'simonblog';
+export class AppComponent implements OnInit {
+
+  src$: Observable<any>;
+
+  constructor() {
+    this.src$ = of('https://cdn.devdojo.com/images/november2020/hero-image.jpeg').pipe(
+      concatMap(url => of(url).pipe())
+    );
+  }
+
+
+  ngOnInit(): void {
+    // this.loadImages();
+    this.typeWords();
+  }
+
+  typeWords(): void {
+    new TypeIt("#apps", {
+      strings: ['Apps', 'and Tools.']
+    }).go();
+  }
+
+  loadImages() {
+
+    this.src$ = of('../assets/img/ds/ds1.jpg', '../assets/img/ds/da2.png').pipe(
+      concatMap(url => of(url).pipe(delay(3000))),
+      repeat()
+   );
+
+   console.log(this.src$);
+  }
+
 }
---
+++ simonblog/tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    prefix: '',
+    purge: {
+      content: [
+        './src/**/*.{html,ts}',
+      ]
+    },
+    darkMode: 'class', // or 'media' or 'class'
+    theme: {
+      extend: {
+        theme: {
+          fontFamily: {
+           'body': [ 'Nunito', 'Roboto', 'sans-serif' ]
+          }
+        }
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [require('@tailwindcss/aspect-ratio'),require('@tailwindcss/forms'),require('@tailwindcss/line-clamp'),require('@tailwindcss/typography')],
+};
